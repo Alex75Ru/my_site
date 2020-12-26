c03591b6e514672cbd0b002ec06afcb5ea1f2e1d
Alex <Asutp.org@gmail.com>
2020-12-27 02:16:35 +0900

    new version learn video

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values': ['Если у вас остались вопросы, обращайтесь по телефону', '555 55 55']})
--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'mainApp',
     'webexample',
     'django.contrib.admin',
